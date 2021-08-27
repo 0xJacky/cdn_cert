aebc17dd35210cd6905d43f6d19ecf631f414763
Jacky <jacky-943572677@qq.com>
2021-08-27 13:50:31 +0800

    修复 Python3.9 邮件错误

--- mail.py
+++ mail.py
@@ -10,14 +10,16 @@ from email.mime.base import MIMEBase
 from email.mime.application import MIMEApplication
 from email.header import Header
 
+
 class Mail:
     def __init__(self):
         # 构建alternative结构
-        self.msg =  MIMEMultipart('alternative')
+        self.msg = MIMEMultipart('alternative')
         self.msg['From'] = '%s <%s>' % (Header(settings.From).encode(), settings.UserName)
-        self.msg['To'] = settings.To
+        self.msg['To'] = str(settings.To)
         self.msg['message_id'] = email.utils.make_msgid()
         self.msg['Date'] = email.utils.formatdate()
+
     def send(self, subject, content):
         self.msg['Subject'] = subject
         # 构建alternative的text/plain部分
@@ -25,14 +27,17 @@ class Mail:
         self.msg.attach(textplain)
         # 发送邮件
         try:
-            client = smtplib.SMTP_SSL()
-            #SMTP普通端口为25或80
+            if settings.Port == 80:
+                client = smtplib.SMTP()
+            else:
+                client = smtplib.SMTP_SSL(settings.Host)
+            # SMTP普通端口为25或80
             client.connect(settings.Host, settings.Port)
-            #开启DEBUG模式
+            # 开启DEBUG模式
             client.set_debuglevel(0)
             client.login(settings.UserName, settings.PassWord)
-            #发件人和认证地址必须一致
-            #备注：若想取到DATA命令返回值,可参考 smtplib 的 sendmaili 封装方法:
+            # 发件人和认证地址必须一致
+            # 备注：若想取到DATA命令返回值,可参考 smtplib 的 sendmaili 封装方法:
             #      使用SMTP.mail/SMTP.rcpt/SMTP.data方法
             client.sendmail(settings.UserName, settings.To, self.msg.as_string())
             client.quit()
@@ -44,10 +49,10 @@ class Mail:
         except smtplib.SMTPSenderRefused as e:
             print('邮件发送失败，发件人被拒绝:', e.smtp_code, e.smtp_error)
         except smtplib.SMTPRecipientsRefused as e:
-            print ('邮件发送失败，收件人被拒绝:', e.smtp_code, e.smtp_error)
+            print('邮件发送失败，收件人被拒绝:', e.smtp_code, e.smtp_error)
         except smtplib.SMTPDataError as e:
-            print ('邮件发送失败，数据接收拒绝:', e.smtp_code, e.smtp_error)
+            print('邮件发送失败，数据接收拒绝:', e.smtp_code, e.smtp_error)
         except smtplib.SMTPException as e:
-            print ('邮件发送失败, ', e.message)
+            print('邮件发送失败, ', e.message)
         except Exception as e:
-            print ('邮件发送异常, ', str(e))
+            print('邮件发送异常, ', str(e))
